eeaf7e5454de755ad4cc16e59f195d6532aa3397
GARY\eduar <eduargabor93@gmail.com>
2024-10-13 14:13:46 -0600

    Routing

---
+++ SICOCA/src/app/modules/sale-point/sale-point.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sale-point',
+  standalone: true,
+  imports: [],
+  templateUrl: './sale-point.component.html',
+  styleUrl: './sale-point.component.css'
+})
+export class SalePointComponent {
+
+}
